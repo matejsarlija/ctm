2e0a946fe932bf3fb2cc99ae385f22acaeaa86fc
matejsarlija <matejsarlija1@gmail.com>
2023-01-27 11:44:08 +0100

    working it out

--- main.py
+++ main.py
@@ -3,23 +3,30 @@ from bs4 import BeautifulSoup
 from datetime import date
 from calendar import monthrange
 
+# todays date info
 today = date.today()
+
+# number for todays date
 today_num = today.day
-#print(today.day)
+# so this will work until months are single digit for now :)
+today_month_num = today.month
 
-if (today_num + 5) < 30:
+# get year
+today_year_num = today.year
 
+# programmatically get the number of days in current month
+days_in_this_month = monthrange(today_year_num, today_month_num)[1]
 
-url = f'https://www.hep.hr/ods/bez-struje/19?dp=rijeka&el=RI&datum=26.01.2023'
+#if today_num > days_in_this_month:
+
+url = f'https://www.hep.hr/ods/bez-struje/19?dp=rijeka&el=RI&datum={today_num}.0{today_month_num}.2023'
 response = requests.get(url)
-
-
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-ulica_divs = soup.find_all('div', class_='ulica')
+street_divs = soup.find_all('div', class_='ulica')
 
-for div in ulica_divs:
+for div in street_divs:
     print(div.text)
 
 myStreet = "Kumičićeva"
