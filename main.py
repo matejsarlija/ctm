d94aee06b66f2bbda0fd833d1d32bfd181786d96
matejsarlija <matejsarlija1@gmail.com>
2023-02-10 10:54:26 +0100

    Gitignore to exclude text files

--- main.py
+++ main.py
@@ -10,16 +10,6 @@ print("The cosine of pi is ", math.cos(math.pi))
 print("In python .math, e is ", math.e)
 print("the log of e is", math.log(math.e))
 
-def movie_review(str_movie):
-    print('Regarding %s - %s'
-          %(str_movie, {x:y for (x,y) in enumerate(["See it!", "A gem!", "Ideological claptrap!"])}[randint(0,2)],))
-
-
-movie_review("Matrix")
-
-def quadratic(a,b,c):
-    discriminant = math.sqrt(b*b - 4*a*c)
-    return ((-b + discriminant)/(2*a), (-b -discriminant)/(2*a))
 
 f = open("input2.txt")
 lines = list(f)
