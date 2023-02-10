c574eac7d1359bf62c7f172d5ac055e00225795e
matejsarlija <matejsarlija1@gmail.com>
2023-02-10 18:14:29 +0100

    finishing up chapter 1

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 
 f = open("input2.txt")
 lines = list(f)
-#print(lines)
-#print([x.split() for x in lines])
+
 def makeInverseIndex(strlist):
     ourDict = {x:set(y.split()) for x,y in enumerate(strlist)}
     inverseIndexDict = {}
 
     for x in ourDict.items():
         for y in x[1]:
-            inverseIndexDict[y] = x[0]
+            if y in inverseIndexDict:
+                inverseIndexDict[y].add(x[0])
+            else:
+                inverseIndexDict[y] = {x[0]}
 
     return inverseIndexDict
 
 print(makeInverseIndex(lines))
 
 def orSearch(inverseIndex, query):
+    for x in query:
+        if x in inverseIndex:
+            return inverseIndex.get(x)
+def andSearch(inverseIndex, query):
     return {inverseIndex[x] for x in query if x in inverseIndex}
 
-print(orSearch(makeInverseIndex(lines), ['ali', 'matej']))
+print(orSearch(makeInverseIndex(lines), ['matej', 'je']))
