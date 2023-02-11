973e51e28c5a59d2c36a986336c4268081ee2b68
matejsarlija <matejsarlija1@gmail.com>
2023-02-11 11:50:40 +0100

    and_search

--- main.py
+++ main.py
@@ -19,17 +19,30 @@ our_index = makeInverseIndex(lines)
 print(our_index)
 
 
-def orSearch(inverseIndex, query):
-    result_dict = set()
+def or_search(inverse_index, query):
+    result_set = set()
 
     for x in query:
-        if x in inverseIndex:
-            result_dict.update(inverseIndex.get(x))
+        if x in inverse_index:
+            result_set.update(inverse_index.get(x))
 
-    return result_dict
+    return result_set
 
 
-def and_search(inverseIndex, query):
-    return {inverseIndex[x] for x in query if x in inverseIndex}
+# Write a procedure andSearch(inverseIndex, query) which takes an inverse
+# index and a list of words query, and returns the set of document numbers specifying
+# all documents that contain all the words in query.
+def and_search(inverse_index, query):
+    result_set = set()
 
-print(orSearch(our_index, ['matej', 'je']))
+    for x in query:
+        if x in inverse_index:
+            if result_set:
+                result_set.intersection_update(inverse_index.get(x))
+            else:
+                result_set.update(inverse_index.get(x))
+
+    return result_set
+
+
+print(and_search(our_index, ['matej', 'ok']))
