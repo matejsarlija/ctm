510ddbfbe61b18a243e1d1f9bb95af7e489a49b1
matejsarlija <matejsarlija1@gmail.com>
2023-02-18 21:27:11 +0100

    a bit gnarly

--- week3.py
+++ week3.py
@@ -1,15 +1,31 @@
 import numpy
 import numpy as np
+from matplotlib import pyplot as plt
+
+
 # Write a python procedure segment(pt1, pt2) that, given points represented as
 # 2-element lists, returns a list of a hundred points spaced evenly along the line segment whose
 # endpoints are the two points
 # Plot the hundred points resulting when pt1 = [3.5, 3] and pt2 = [0.5, 1]
 def segment(pt1, pt2):
-   segment_params =  [[x,y] for x,y in zip(np.arange(0,1,.01), list(reversed(np.arange(0,1,.01))))]
-   for x in segment_params:
-      print(x[0]*pt1[0]+x[1]*pt2[0],x[0]*pt1[1]+x[1]*pt2[1])
+    segment_params = [[x, y] for x, y in zip(np.arange(0, 1, .01), list(reversed(np.arange(0, 1, .01))))]
 
-a = [3,5]
+    points = []
+
+    for x in segment_params:
+        point = [x[0] * pt1[0] + x[1] * pt2[0], x[0] * pt1[1] + x[1] * pt2[1]]
+        points.append(point)
+
+    return points
+
+
+a = [3, 5]
 b = [0.5, 1]
-print([[x,y] for x,y in zip(np.arange(0,1,.01), list(reversed(np.arange(0,1,.01))))])
-print(segment(a, b))
+# print([[x, y] for x, y in zip(np.arange(0, 1, .01), list(reversed(np.arange(0, 1, .01))))])
+
+points = segment(a, b)
+print(points)
+
+x_coords, y_coords = zip(*points)
+plt.plot(x_coords, y_coords, 'o')
+plt.show()
