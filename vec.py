4415c862a3129f398d0e3b29bc836a8697245581
matejsarlija <matejsarlija1@gmail.com>
2023-03-12 14:55:47 +0100

    extractic get and set to not be in Vec class

--- vec.py
+++ vec.py
@@ -3,13 +3,8 @@ class Vec:
         self.D = labels
         self.f = function
 
-    def setitem(self, d, val):
-        self.f[d] = val
 
-    def getitem(self, d):
-        return self.f[d] if d in self.f else 0
-    
-    
+
 
 
 
--- week3.py
+++ week3.py
@@ -25,14 +25,30 @@ def segment(pt1, pt2):
 def zero_vec(D):
     return Vec(D, {x:0 for x in D})
 
+def getitem(v, d):
+    return v.f[d] if d in v.f else 0
+
+def setitem(v, d, val):
+    v.f[d] = val
+
 # Quiz 2.7.3: Write a procedure scalar_mul(v, alpha) with the following spec:
 # • input: an instance of Vec and a scalar alpha
 # • output: a new instance of Vec that represents the scalar-vector product alpha times v.
 def scalar_mul(v, alpha):
     return Vec(v.D, {x:y*alpha for (x,y) in v.f.items()})
 
-v = Vec({'A','B','C'}, {'A':1, 'B':2, 'C':4})
-x = v.getitem('A')
-print((scalar_mul(v, 3)).f)
+# Quiz 2.7.4: Write a procedure add(u, v) with the following spec:
+# • input: instances u and v of Vec
+# • output: an instance of Vec that is the vector sum of u and v
+def add(u, v):
+    return Vec(u.D, {x:getitem(u, x) + getitem(v, x) for x in v.D})
+
+
+v = Vec({'A','B','C'}, {'A':1, 'B':2})
+u = Vec(v.D, {'A':5., 'C':10.})
+z = Vec({'Z'}, {'Z':1})
+x = getitem(v, 'A')
+#print((scalar_mul(v, 3)).f)
+print((add(z, v)).f)
 
 #print(x)
